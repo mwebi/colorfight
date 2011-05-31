2583bc15ba93ecde45f11be02d0d5b8195fbc9fd
fhs31481 <mwebersdorfer.mmt-b2009@fh-salzburg.ac.at>
2011-05-31 15:12:50 +0200

    photo and color reading works

---
+++ src/ar2/com/ar2.java
@@ -0,0 +1,185 @@
+/*package ar2.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ar2 extends Activity {
+    // Called when the activity is first created.
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}*/
+
+package ar2.com;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.PixelFormat;
+import android.graphics.Bitmap.CompressFormat;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+public class ar2 extends Activity {
+	private SurfaceView preview=null;
+	private SurfaceHolder previewHolder=null;
+	private Camera mCamera=null;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.d("color", "onCreate");
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		preview=(SurfaceView)findViewById(R.id.surface_camera); 
+		previewHolder=preview.getHolder();
+		previewHolder.addCallback(surfaceCallback);
+		previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		Log.d("color", "onCreate end");
+	}
+	
+	
+	
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+		//-- When we touch the screen, it will auto-focus again
+		Log.d("color", "dispatchTouchEvent");
+		if(mCamera != null){
+			mCamera.cancelAutoFocus(); //release the previous auto-focus
+			mCamera.autoFocus(new Camera.AutoFocusCallback(){
+				@Override
+				public void onAutoFocus(boolean success, Camera camera) {
+					Log.d("HOME", "isAutofoucs " + Boolean.toString(success));					
+				}
+			} );
+			
+		}
+		
+		return super.dispatchTouchEvent(ev);
+		
+	
+	}
+
+	//--Define which keys will "take the picture"
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		Log.d("color", "onKeyDown: " + keyCode);
+
+		if (keyCode==KeyEvent.KEYCODE_CAMERA ||	keyCode==KeyEvent.KEYCODE_SEARCH ||	keyCode==23) {
+			takePicture();
+			
+			return(true);
+		}
+		
+		return(super.onKeyDown(keyCode, event));
+	}
+	
+	private void takePicture() {
+		mCamera.takePicture(null, null, photoCallback);
+	}
+	
+	SurfaceHolder.Callback surfaceCallback=new SurfaceHolder.Callback() {
+		
+		
+		public void surfaceCreated(SurfaceHolder holder) {
+			Log.d("color", "surfaceCreated");
+			mCamera=Camera.open();
+			Log.d("color", "surfaceCreated nach open");
+			try {
+				mCamera.setPreviewDisplay(previewHolder);
+			}
+			catch (Throwable t) {
+				Log.e("PictureDemo-surfaceCallback", "Exception in setPreviewDisplay()", t);
+				Log.d("color", "Exception in setPreviewDisplay()", t);
+				Toast
+					.makeText(ar2.this, t.getMessage(), Toast.LENGTH_LONG)
+					.show();
+			}
+			Log.d("color", "surfaceCreated end");
+		}
+		
+		public void surfaceChanged(SurfaceHolder holder,int format, int width, int height) {
+			Log.d("color", "surfaceChanged");
+			Camera.Parameters parameters=mCamera.getParameters();
+			
+			//Customize width/height here - otherwise defaults to screen width/height
+			parameters.setPreviewSize(width, height);
+			parameters.setPictureFormat(PixelFormat.JPEG);
+			parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+			parameters.setJpegQuality(100);
+
+			//mCamera.setParameters(parameters);
+			mCamera.startPreview();
+			
+			Log.d("color", "pre autofocus");
+			//-- Must add the following callback to allow the camera to autofocus.
+			mCamera.autoFocus(new Camera.AutoFocusCallback(){
+				@Override
+				public void onAutoFocus(boolean success, Camera camera) {
+					Log.d("HOME", "isAutofoucs " + Boolean.toString(success));					
+				}
+			} );
+			Log.d("color", "surfaceChanged end");
+		}
+		
+		public void surfaceDestroyed(SurfaceHolder holder) {
+
+		}
+	};
+	
+	Camera.PictureCallback photoCallback=new Camera.PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			Log.d("color", "photoCallback");
+			Intent mIntent = new Intent();
+			mIntent.putExtra("picture", data);
+			setResult(1, mIntent); //-- 1 for result code = ok
+			
+			Bitmap pic = BitmapFactory.decodeByteArray(data, 0, data.length);
+			Color mColor;
+
+			for(int x=0; x<10; x++){
+				for(int y=0; y<10; y++){
+					int red = pic.getPixel(x, y) & 0x00FF0000 >> 16 ;
+					Log.d("color", "color: " + red );
+					
+				}
+			}
+			
+			for(int i=0; i<data.length; i++){
+				//Log.d("color", "i: " + data[i]);
+			}
+			
+			//-- This segment moved from surfaceDestroyed - otherwise the Camera is not properly released
+			if(mCamera != null){
+				mCamera.stopPreview();
+				mCamera.release();
+				mCamera=null;
+			}
+
+			finish();
+			Log.d("color", "photoCallback end");
+		}
+	};
+	
+
+}
